f8c0cc69fce1aaa5989243d0edb341328effb678
pyw31337 <pyw31337@users.noreply.github.com>
2026-08-14 11:51:46 +0900

    Split domain constants into shared file

--- public/assets/app-constants.js
+++ public/assets/app-constants.js
@@ -30,9 +30,24 @@
     etc: '💬'
   });
 
+  const SCHEDULE_ACTIVITY_ACTIONS = Object.freeze(['create', 'update', 'delete']);
+  const POLL_ACTIVITY_ACTIONS = Object.freeze(['poll_create', 'poll_vote', 'poll_cancel']);
+  const EXPENSE_ACTIVITY_ACTIONS = Object.freeze(['expense_create', 'expense_update', 'expense_delete']);
+  const IMAGE_TAG_ACTIVITY_ACTIONS = Object.freeze(['tag_add', 'tag_remove']);
+  const MEETING_ACTIVITY_ACTIONS = Object.freeze(['meeting_confirm', 'meeting_cancel']);
+  const MEMO_ACTIVITY_ACTIONS = Object.freeze(['memo_create', 'memo_update', 'memo_delete']);
+  const CALENDAR_ACCENT_PALETTE = Object.freeze(['#F97316', '#3B82F6', '#8B5CF6', '#10B981', '#EC4899', '#F59E0B']);
+
   window.GATHER_APP_CONSTANTS = Object.freeze({
     PRESET_COLORS,
     DEFAULT_EXPENSE_CATEGORIES,
-    EXPENSE_CATEGORY_ICONS
+    EXPENSE_CATEGORY_ICONS,
+    SCHEDULE_ACTIVITY_ACTIONS,
+    POLL_ACTIVITY_ACTIONS,
+    EXPENSE_ACTIVITY_ACTIONS,
+    IMAGE_TAG_ACTIVITY_ACTIONS,
+    MEETING_ACTIVITY_ACTIONS,
+    MEMO_ACTIVITY_ACTIONS,
+    CALENDAR_ACCENT_PALETTE
   });
 })();
